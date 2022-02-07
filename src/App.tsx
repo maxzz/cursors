005da99b7c0290929bcc028be3f8381ae41125e8
Max Zakharzhevskiy <maxzz@msn.com>
2022-02-07 01:45:47 -0800

    WIP. add initial dropped file content handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,75 @@
 import { useRef, useState } from 'react';
 import './App.css';
 
+function loadImage(reader: FileReader) {
+    // const lines = reader.result.split('\n').map(function (line) { return line.split(','); }); console.log(lines);
+    if (!reader.result) {
+        return;
+    }
+    try {
+        const img = new Image();
+        if (!img) {
+            return;
+        }
+
+        img.onload = function () {
+            const canvas = document.createElement('canvas');
+            const context = canvas.getContext('2d');
+            if (!context) {
+                return;
+            }
+            context.drawImage(img, 0, 0);
+
+            const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+            const data = imageData.data;
+            for (var i = 0; i <= data.length; i += 4) {
+                const avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
+                data[i] = avg;
+                data[i + 1] = avg;
+                data[i + 2] = avg;
+            }
+            context.putImageData(imageData, 0, 0);
+
+            document.body.appendChild(canvas);
+
+            //canvas.toDataURL()
+            //const csvfile = new Blob(['one,two,three'], { type: 'text/csv' })
+
+            /*
+            canvas.toBlob(function (blob) {
+                if (!blob) {
+                    return;
+                }
+
+                const form = new FormData();
+                form.append('image', blob, 'moody.jpg');
+
+                const xhr = new XMLHttpRequest();
+                xhr.open('POST', '/imageupload', true);
+                xhr.send(form);
+            });
+            */
+        };
+
+        img.src = reader.result.toString();
+        //document.body.appendChild(img)
+
+    } catch (error) {
+
+    }
+}
+
+function handleDrop(files: FileList) {
+    console.log('onDrop', files);
+
+    const reader = new FileReader();
+    reader.onload = () => loadImage(reader);
+
+    //reader.readAsText(files[0])
+    reader.readAsDataURL(files[0]);
+
+}
+
 function DropZone() {
     const inputRef = useRef<HTMLInputElement>(null);
     const [dropActive, setDropActive] = useState(false);
@@ -10,26 +79,21 @@ function DropZone() {
             onDragOver={(event) => {
                 event.preventDefault();
                 event.stopPropagation();
-
-                console.log('onDragOver');
-                setDropActive(true);
+                !dropActive && setDropActive(true);
             }}
             onDrop={(event) => {
                 event.preventDefault();
                 event.stopPropagation();
-                console.log('onDrop', event.dataTransfer.files);
                 setDropActive(false);
+                handleDrop(event.dataTransfer.files);
             }}
         >
             <input
                 ref={inputRef}
                 type="file"
                 accept='.png'
-                className={`w-full h-full hidden`}
-                onChange={(event) => {
-                    console.log('change', inputRef.current, inputRef.current?.files);
-                    setDropActive(false);
-                }}
+                className="hidden"
+                onChange={(event) => inputRef.current?.files && handleDrop(inputRef.current?.files)}
             />
         </label>
     );
