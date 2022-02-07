6e6e743a983dec93c28beb8163c6fcb9fa207b4a
Max Zakharzhevskiy <maxzz@msn.com>
2022-02-07 01:59:03 -0800

    WIP. fix canvas creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { useRef, useState } from 'react';
+import { RefObject, useRef, useState } from 'react';
 import './App.css';
 
-function loadImage(reader: FileReader) {
+function loadImage(reader: FileReader, canvasRef: RefObject<HTMLCanvasElement>) {
     // const lines = reader.result.split('\n').map(function (line) { return line.split(','); }); console.log(lines);
-    if (!reader.result) {
+    if (!reader.result || !canvasRef.current) {
         return;
     }
+
     try {
         const img = new Image();
         if (!img) {
@@ -13,7 +14,11 @@ function loadImage(reader: FileReader) {
         }
 
         img.onload = function () {
-            const canvas = document.createElement('canvas');
+            //const canvas = document.createElement('canvas');
+            const canvas = canvasRef.current;
+            if (!canvas) {
+                return;
+            }
             const context = canvas.getContext('2d');
             if (!context) {
                 return;
@@ -30,7 +35,7 @@ function loadImage(reader: FileReader) {
             }
             context.putImageData(imageData, 0, 0);
 
-            document.body.appendChild(canvas);
+            //document.body.appendChild(canvas);
 
             //canvas.toDataURL()
             //const csvfile = new Blob(['one,two,three'], { type: 'text/csv' })
@@ -59,11 +64,11 @@ function loadImage(reader: FileReader) {
     }
 }
 
-function handleDrop(files: FileList) {
+function handleDrop(files: FileList, canvasRef: RefObject<HTMLCanvasElement>) {
     console.log('onDrop', files);
 
     const reader = new FileReader();
-    reader.onload = () => loadImage(reader);
+    reader.onload = () => loadImage(reader, canvasRef);
 
     //reader.readAsText(files[0])
     reader.readAsDataURL(files[0]);
@@ -72,30 +77,35 @@ function handleDrop(files: FileList) {
 
 function DropZone() {
     const inputRef = useRef<HTMLInputElement>(null);
+    const canvasRef = useRef<HTMLCanvasElement>(null);
     const [dropActive, setDropActive] = useState(false);
     return (
-        <label
-            className={`w-32 h-16 inline-block border-slate-500 border rounded ${dropActive ? 'bg-pink-400' : 'bg-slate-400'}`}
-            onDragOver={(event) => {
-                event.preventDefault();
-                event.stopPropagation();
-                !dropActive && setDropActive(true);
-            }}
-            onDrop={(event) => {
-                event.preventDefault();
-                event.stopPropagation();
-                setDropActive(false);
-                handleDrop(event.dataTransfer.files);
-            }}
-        >
-            <input
-                ref={inputRef}
-                type="file"
-                accept='.png'
-                className="hidden"
-                onChange={(event) => inputRef.current?.files && handleDrop(inputRef.current?.files)}
-            />
-        </label>
+        <>
+            <label
+                className={`w-32 h-16 inline-block border-slate-500 border rounded ${dropActive ? 'bg-pink-400' : 'bg-slate-400'}`}
+                onDragOver={(event) => {
+                    event.preventDefault();
+                    event.stopPropagation();
+                    !dropActive && setDropActive(true);
+                }}
+                onDrop={(event) => {
+                    event.preventDefault();
+                    event.stopPropagation();
+                    setDropActive(false);
+                    handleDrop(event.dataTransfer.files, canvasRef);
+                }}
+            >
+                <input
+                    ref={inputRef}
+                    type="file"
+                    accept='.png'
+                    className="hidden"
+                    onChange={(event) => inputRef.current?.files && handleDrop(inputRef.current?.files, canvasRef)}
+                />
+            </label>
+            <canvas ref={canvasRef} className="bg-slate-300" />
+            {/* <canvas ref={canvasRef} className="w-40 h-32 bg-slate-300" /> */}
+        </>
     );
 }
 
