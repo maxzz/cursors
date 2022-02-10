42a538c8e4712f58859757fbbbbabfe03cae2355
Max Zakharzhevskiy <maxzz@msn.com>
2022-02-10 15:50:30 -0800

    WIP. update canvas atom to derived

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,7 +3,14 @@ import { atom } from "jotai";
 // canvas
 
 export const orgImgAtom = atom<HTMLImageElement | null>(null);
-export const canvasAtom = atom<HTMLCanvasElement | null>(null);
+
+const _canvasAtom = atom<HTMLCanvasElement | null>(null);
+export const canvasAtom = atom(
+    (get) => get(_canvasAtom),
+    (get, set, value: HTMLCanvasElement | null) => {
+        set(_canvasAtom, value);
+    }
+);
 export const canvasCtxAtom = atom<CanvasRenderingContext2D | null>(null);
 
 // options
