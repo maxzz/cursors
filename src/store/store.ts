4077e58f20cae79a15fd24cbf0876ff66c9cc617
Max Zakharzhevskiy <maxzz@msn.com>
2022-02-11 15:52:29 -0800

    WIP. add canvasBodyAtom atom update function

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,4 @@
-import { atom, SetStateAction } from "jotai";
+import { atom, Getter, PrimitiveAtom, SetStateAction, Setter } from "jotai";
 
 export type ViewPoint = { x: number; y: number; };
 export type ViewSize = { w: number; h: number; };
@@ -17,18 +17,33 @@ type CanvasBody = {
 
 const _canvasBodyAtom = atom<CanvasBody | null | undefined>(null);
 
+function atomCanvasUpdate(get: Getter, set: Setter, value: SetStateAction<HTMLCanvasElement | null | undefined>, atomToSet: PrimitiveAtom<CanvasBody | null | undefined>) {
+    let newBody: CanvasBody | null | undefined;
+    if (value) {
+        let el = typeof value === 'function' ? value(get(atomToSet)?.el) : value;
+        let ctx = value && el?.getContext('2d');
+        newBody = el && ctx && { el, ctx, };
+    }
+    set(atomToSet, newBody);
+}
+
 export const canvasBodyAtom = atom(
     (get) => get(_canvasBodyAtom),
-    (get, set, value: SetStateAction<HTMLCanvasElement | null | undefined>) => {
-        let newBody: CanvasBody | null | undefined;
-        if (value) {
-            let el = typeof value === 'function' ? value(get(_canvasBodyAtom)?.el) : value;
-            let ctx = value && el?.getContext('2d');
-            newBody = el && ctx && { el, ctx, };
-        }
-        set(_canvasBodyAtom, newBody);
-    }
+    (get, set, value: SetStateAction<HTMLCanvasElement | null | undefined>) => ((atom) => atomCanvasUpdate(get, set, value, atom))(_canvasBodyAtom)
 );
+
+// export const canvasBodyAtom = atom(
+//     (get) => get(_canvasBodyAtom),
+//     (get, set, value: SetStateAction<HTMLCanvasElement | null | undefined>) => {
+//         let newBody: CanvasBody | null | undefined;
+//         if (value) {
+//             let el = typeof value === 'function' ? value(get(_canvasBodyAtom)?.el) : value;
+//             let ctx = value && el?.getContext('2d');
+//             newBody = el && ctx && { el, ctx, };
+//         }
+//         set(_canvasBodyAtom, newBody);
+//     }
+// );
 
 // options
 
