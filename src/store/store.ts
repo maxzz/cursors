d5f85027fc951862e639f5938b41e4c88aacdfbe
Max Zakharzhevskiy <maxzz@msn.com>
2022-02-11 14:33:24 -0800

    WIP. add canvas size atom

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,16 @@
 import { atom } from "jotai";
 
+export type ViewPoint = { x: number; y: number; };
+export type ViewSize = { w: number; h: number; };
+export type ViewBox = ViewPoint & ViewSize;
+
 // canvas
 
 export const orgImgAtom = atom<HTMLImageElement | null>(null);
 
 const _canvasAtom = atom<HTMLCanvasElement | null>(null);
+export const canvasCtxAtom = atom<CanvasRenderingContext2D | null>(null);
+
 export const canvasAtom = atom(
     (get) => get(_canvasAtom),
     (get, set, el: HTMLCanvasElement | null) => {
@@ -17,7 +23,19 @@ export const canvasAtom = atom(
     }
 );
 
-export const canvasCtxAtom = atom<CanvasRenderingContext2D | null>(null);
+export const canvasSizeAtom = atom(
+    (get) => {
+        const c = get(_canvasAtom);
+        return c ? { w: c.width, h: c.height } : { w: 0, h: 0 };
+    },
+    (get, set, size: ViewSize) => {
+        const c = get(_canvasAtom);
+        if (c) {
+            c.width = size.w;
+            c.height = size.h;
+        }
+    }
+);
 
 // options
 
