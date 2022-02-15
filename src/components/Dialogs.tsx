ccea748402dcc0eb2c1d952f16a9cc3dc4b00be4
Max Zakharzhevskiy <maxzz@msn.com>
2022-02-15 15:52:45 -0800

    WIP. add help dialog temp content

--- src/components/Dialogs.tsx
+++ src/components/Dialogs.tsx
@@ -2,13 +2,25 @@ import React from 'react';
 import { useAtom } from 'jotai';
 import { showHelpIdAtom } from '../store/store';
 import { PortalModal } from './UI/UIDialog';
+import { classNames } from '../utils/classnames';
 
 function FormHelp({ editorData, setShow = (v: boolean) => { } }: { editorData: number; setShow?: (v: boolean) => void; }) {
     return (
-        <div className="w-[460px] h-[640px] bg-gray-200 rounded overflow-hidden">
+        <div className={classNames(
+            "max-w-[460px] max-h-[640px] bg-gray-200 rounded overflow-hidden", //w-[460px] h-[640px]
+            "grid grid-rows-[min-content,1fr,min-content]")}
+        >
+            {/* Header */}
             <div className="px-2 py-3 text-xl font-bold leading-tight tracking-tight border-slate-500 border-b">Help</div>
-            <div className="">Show help for topic {editorData}</div>
-            <button className="" onClick={() => setShow(false)}>Close</button>
+            {/* Body */}
+            <div className="min-w-[16rem] min-h-[6rem] p-2 text-sm">
+                <div className="">TODO: Show help for topic: {editorData}</div>
+                <div className="">TODO: github link</div>
+            </div>
+            {/* Buttons */}
+            <div className="p-3 flex items-center justify-end">
+                <button className="px-3 py-2 border-slate-500 border rounded" onClick={() => setShow(false)}>Close</button>
+            </div>
         </div>
     );
 }
