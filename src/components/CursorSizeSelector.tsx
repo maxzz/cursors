6de596c0af631d0c8b8064e8fee4faf8bd78a3e3
Max Zakharzhevskiy <maxzz@msn.com>
2022-02-21 01:25:37 -0800

    WIP. fix dropdown background color bleading

--- src/components/CursorSizeSelector.tsx
+++ src/components/CursorSizeSelector.tsx
@@ -14,11 +14,15 @@ export function CursorSizeSelector() {
     const styles = useSpring({ open: open ? 1 : 0, config: { mass: 0.2, tension: 492, clamp: true } });
     return (
         <div className="relative inline-block text-xs">
-            <label className={classNames("w-16 flex items-center bg-slate-300 border-slate-500 border overflow-hidden focus-within:ring", open ? 'rounded-t-md' : 'rounded-md')}>
+            <label className={classNames(
+                "w-16 flex items-center bg-slate-400 border-slate-500 border overflow-hidden focus-within:ring shadow",
+                open ? 'rounded-t-md' : 'rounded-md'
+            )}>
                 <div className="w-10">
-                    <input className="w-full px-2 py-1 bg-transparent border-slate-500 border-r focus:outline-none" type="text" {...bind} />
+                    <input className="w-full px-2 py-1 bg-slate-300 border-slate-500 border-r focus:outline-none" type="text" {...bind} />
                 </div>
-                <button className="focus:outline-none focus:bg-slate-400" onClick={() => setOpen(v => !v)}>
+                {/* <button className="focus:outline-none focus:bg-slate-400" onClick={() => setOpen(v => !v)}> */}
+                <button className="focus:outline-none bg-slate-300 focus:bg-slate-400" onClick={() => setOpen(v => !v)}>
                     {/* Open/Close icon */}
                     <svg className="w-6 h-6 p-1 stroke-current stroke-[.6rem] fill-transparent" viewBox="0 0 100 100">
                         <a.path d={styles.open.to({ range: [0, 1], output: ["M 15 34 L 45 65 L 78 34", "M 15 53 L 45 23 L 78 53"] })} />
@@ -26,7 +30,7 @@ export function CursorSizeSelector() {
                 </button>
             </label>
             {open &&
-                <div className="absolute top-full w-16 bg-slate-300 border-slate-500 border rounded-b-md">
+                <div className="absolute top-full w-16 bg-slate-300 border-slate-500 border border-t-0 rounded-b-md">
                     {CURSOR_SIZES2.map((itemSize) => (
                         <div
                             className={`w-full px-2 py-1 hover:bg-slate-400 ${itemSize === size ? 'font-bold' : ''} cursor-pointer`}
