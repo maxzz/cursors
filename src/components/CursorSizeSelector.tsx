b03182449a07021747f2859d28ca08adcea7a1c8
Max Zakharzhevskiy <maxzz@msn.com>
2022-02-21 18:44:49 -0800

    WIP. dropdown aniamtion. code clean up

--- src/components/CursorSizeSelector.tsx
+++ src/components/CursorSizeSelector.tsx
@@ -30,21 +30,27 @@ export function CursorSizeSelector() {
     const styles = useSpring({ open: open ? 1 : 0, config: { mass: 0.2, tension: 492, clamp: true } });
     return (
         <div className="relative inline-block text-xs">
-            <label className={classNames(
-                "w-16 flex items-center bg-slate-400 border-slate-500 border overflow-hidden focus-within:ring shadow-md",
-                open ? 'rounded-t-md' : 'rounded-md'
-            )}>
+
+            {/* Input and button */}
+            <label className={
+                classNames(
+                    "w-16 flex items-center bg-slate-400 border-slate-500 border overflow-hidden focus-within:ring shadow-md",
+                    open ? 'rounded-t-md' : 'rounded-md'
+                )}
+            >
                 <div className="w-10">
                     <input className="w-full px-2 py-1 text-center bg-slate-300 border-slate-500 border-r focus:outline-none" type="text" {...bind} />
                 </div>
-                {/* <button className="focus:outline-none focus:bg-slate-400" onClick={() => setOpen(v => !v)}> */}
+
+                {/* Open/Close icon */}
                 <button className="focus:outline-none bg-slate-300 focus:bg-slate-400" onClick={() => setOpen(v => !v)}>
-                    {/* Open/Close icon */}
                     <svg className="w-6 h-6 p-1 stroke-current stroke-[.6rem] fill-transparent" viewBox="0 0 100 100">
                         <a.path d={styles.open.to({ range: [0, 1], output: ["M 15 34 L 45 65 L 78 34", "M 15 53 L 45 23 L 78 53"] })} />
                     </svg>
                 </button>
             </label>
+
+            {/* List */}
             {open && <DropDown size={size} setSize={setSize} setOpen={setOpen} />}
         </div>
     );
