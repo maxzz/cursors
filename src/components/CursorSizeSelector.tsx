6f39ca2c8227946f004d7e4bccdd80eaf9d4efa8
Max Zakharzhevskiy <maxzz@msn.com>
2022-02-23 03:00:24 -0800

    WIP. dropdown aniamtion. test keyboard support

--- src/components/CursorSizeSelector.tsx
+++ src/components/CursorSizeSelector.tsx
@@ -5,23 +5,38 @@ import { classNames } from "../utils/classnames";
 import { cleanupValueUInt, useNumberInput } from "../hooks/useNumberInput";
 import { UIListTransition } from "./UI/UIListTransition";
 import { UIIconUpDown } from "./UI/UIIconUpDown";
-import { useClickAway } from "react-use";
+import { useClickAway, useKey } from "react-use";
 
 const CURSOR_SIZES2 = [16, 32, 128, 256];
 
 function DropDownList({ size, setSize, setOpen }: { size: number, setSize: (v: number) => void, setOpen: (v: boolean) => void; }) {
+    const containerRef = React.useRef<HTMLUListElement>(null);
+    function handleKey(event: KeyboardEvent) {
+        console.log('k');
+    }
+    useKey((event) => {
+        console.log('ee', event);
+        
+        return event.key === 'ArrowUp'
+    }, handleKey, { target: containerRef.current });
+    console.log('ini', containerRef.current);
+    
     return (
-        <div className="absolute top-full w-16 bg-slate-400 border-slate-500 border border-t-0 rounded-b-md overflow-hidden">
+        <ul className="absolute top-full w-16 bg-slate-400 border-slate-500 border border-t-0 rounded-b-md overflow-hidden" ref={containerRef} tabIndex={0}
+        // onKeyUpCapture={(event) => {
+        //     console.log('ee inline', event);
+        // }}
+        >
             {CURSOR_SIZES2.map((itemSize) => (
-                <div
+                <li
                     className={`w-full px-2 py-1 bg-slate-300 hover:bg-slate-400 ${itemSize === size ? 'font-bold' : ''} cursor-pointer`}
                     key={itemSize}
                     onClick={() => { setSize(itemSize); setOpen(false); }}
                 >
                     {itemSize}
-                </div>
+                </li>
             ))}
-        </div>
+        </ul>
     );
 }
 
