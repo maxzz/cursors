23011ada26e83557de3282e9081673aaabb22e01
Max Zakharzhevskiy <maxzz@msn.com>
2022-02-27 16:32:52 -0800

    WIP. dropdown. fix wrong target on list and button enter

--- src/components/CursorSizeSelector.tsx
+++ src/components/CursorSizeSelector.tsx
@@ -10,28 +10,27 @@ import { useClickAway } from "react-use";
 const CURSOR_SIZES2 = [16, 32, 128, 256];
 
 function DropDownList({ size, setSize, setOpen }: { size: number, setSize: (v: number) => void, setOpen: (v: boolean) => void; }, ref: React.Ref<HTMLUListElement>) {
-    function handleKey(event: React.KeyboardEvent<HTMLUListElement>) {
-        //event.preventDefault();
-        const k = event.key;
-
-        console.log('-----List:onKeyDown key', k, event, document.activeElement);
-
-        if (k === 'ArrowUp' || k === 'ArrowDown') {
-            let idx = CURSOR_SIZES2.findIndex((item) => item === size);
-            idx = idx !== -1 ? idx + (k === 'ArrowUp' ? -1 + CURSOR_SIZES2.length : 1) : 0;
-            setSize(CURSOR_SIZES2[idx % CURSOR_SIZES2.length]);
-        }
-        else if (k === 'Enter' || k === 'Escape' || k === 'Space') {
-            setOpen(false);
-        }
-    }
     return (
         <ul className="absolute top-full w-16 bg-slate-400 border-slate-500 border border-t-0 rounded-b-md overflow-hidden focus:outline-none focus-within:ring"
             ref={ref}
             tabIndex={0}
-            onKeyDown={handleKey}
+            onKeyDown={function handleKey(event: React.KeyboardEvent<HTMLUListElement>) {
+                event.preventDefault();
+                const k = event.key;
+
+                console.log('📳📳📳List:onKeyDown', { act: document.activeElement?.tagName.toLowerCase(), ev: event.key });
+
+                if (k === 'ArrowUp' || k === 'ArrowDown') {
+                    let idx = CURSOR_SIZES2.findIndex((item) => item === size);
+                    idx = idx !== -1 ? idx + (k === 'ArrowUp' ? -1 + CURSOR_SIZES2.length : 1) : 0;
+                    setSize(CURSOR_SIZES2[idx % CURSOR_SIZES2.length]);
+                }
+                else if (k === 'Enter' || k === 'Escape' || k === 'Space') {
+                    setOpen(false);
+                }
+            }}
             onKeyPress={(event) => {
-                console.log('-----List:onKeyPress', event, document.activeElement);
+                console.log('📳📳📳List:onKeyPress', { act: document.activeElement?.tagName.toLowerCase(), ev: event.key });
             }}
         >
             {CURSOR_SIZES2.map((itemSize) => (
@@ -58,13 +57,13 @@ export function CursorSizeSelector() {
     const buttonRef = React.useRef<HTMLButtonElement>(null);
     useClickAway(containerRef, () => setOpen(false));
     React.useEffect(() => {
-        console.log('useEffect:open', open, 'dropdownRef.current', dropdownRef.current);
+        console.log(`💦💦💦Container.useEffect[open] open=${open}`, 'dropdownRef', { el: dropdownRef.current?.tagName.toLowerCase() });
         if (open) {
             dropdownRef.current?.focus();
         }
     }, [open]);
     function setOpenWFocus(v: boolean) {
-        console.log('setOpenWFocus ref', (v ? dropdownRef : buttonRef).current);
+        console.log(`💦💦💦Container.setOpenWFocus ref=${v ? 'list' : 'button'}`, { el: (v ? dropdownRef : buttonRef).current?.tagName.toLowerCase() });
 
         if (v) {
             dropdownRef.current?.focus();
@@ -94,29 +93,29 @@ export function CursorSizeSelector() {
                     ref={buttonRef}
 
                     onClick={(event) => {
-                        console.log('button:onClick', event);
+                        console.log('💦💦💦Container.button:onClick', { ev: event });
                         setOpenWFocus(!open);
                     }}
-                    /*
-                    onKeyDown={(event) => {
-                        console.log('button:onKeyDown', event);
+                /*
+                onKeyDown={(event) => {
+                    console.log('button:onKeyDown', {ev: event});
 
-                        // event.preventDefault();
-                        // event.stopPropagation();
+                    // event.preventDefault();
+                    // event.stopPropagation();
 
-                        const k = event.key;
-                        if (k === 'Enter' || k === 'Space') {
-                            setOpenWFocus(!open);
-                        }
-                        //setOpen(v => !v);
-                    }}
-                    */
+                    const k = event.key;
+                    if (k === 'Enter' || k === 'Space') {
+                        setOpenWFocus(!open);
+                    }
+                    //setOpen(v => !v);
+                }}
+                */
 
                 // onKeyDown={(event) => {
-                //     console.log('button:onKeyDown', event);
+                //     console.log('button:onKeyDown', {ev: event});
                 // }}
                 // onKeyUp={(event) => {
-                //     console.log('button:onKeyUp', event);
+                //     console.log('button:onKeyUp', {ev: event});
                 // }}
                 >
                     <UIIconUpDown open={open} />
