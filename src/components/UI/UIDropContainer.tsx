4e419fbce5456f9765ccfa0f575d4aa54e4fb7df
Max Zakharzhevskiy <maxzz@msn.com>
2022-12-05 13:04:30 -0800

    WIP. Part1_TopControls. update UIDropContainer

--- src/components/UI/UIDropContainer.tsx
+++ src/components/UI/UIDropContainer.tsx
@@ -1,39 +1,44 @@
-import { useRef } from "react";
+import { HTMLAttributes, useRef } from "react";
 import { PrimitiveAtom, useAtom } from "jotai";
 import { classNames } from "@/utils/classnames";
 
-export function UIDropContainer({ onDropped, accept, className, children, activeAtom, ...rest }: {
-    onDropped: (files: FileList) => {};
+type UIDropContainerProps = {
+    onDropped: (files: FileList) => void;
     accept?: string; // accept = '.png'
     activeAtom: PrimitiveAtom<boolean>;
-} & React.HTMLAttributes<HTMLLabelElement>
-) {
-    const inputRef = useRef<HTMLInputElement>(null);
+};
+
+export function UIDropContainer({ onDropped, accept, className, children, activeAtom, ...rest }: UIDropContainerProps & HTMLAttributes<HTMLLabelElement>) {
     const [dropActive, setDropActive] = useAtom(activeAtom);
+    const inputRef = useRef<HTMLInputElement>(null);
     return (
         <label
             className={classNames(`inline-block`, className)}
+
             onDragOver={(event) => {
                 event.preventDefault();
                 event.stopPropagation();
                 !dropActive && setDropActive(true);
             }}
+
             onDrop={(event) => {
                 event.preventDefault();
                 event.stopPropagation();
                 setDropActive(false);
                 onDropped(event.dataTransfer.files);
             }}
+
             onDragEnter={(event) => setDropActive(true)}
             onDragLeave={(event) => setDropActive(false)}
+
             {...rest}
         >
             <input
+                ref={inputRef}
                 type="file"
                 className="hidden"
                 accept={accept}
                 onChange={() => inputRef.current?.files && onDropped(inputRef.current?.files)}
-                ref={inputRef}
             />
             {children}
         </label>
