d3b8c461e475728a949c7a5962247750bef6ff91
Max Zakharzhevskiy <maxzz@msn.com>
2022-12-05 16:24:43 -0800

    WIP. DropZone. rollback new temp prototype

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { UIToaster } from './components/UI/UiToaster';
 import { Dialogs } from './components/Section7_Dialogs';
 import { Mount } from '@ui/TestTransitions'; // test transitions
 import './App.css';
-import { DropZone2 } from '@ui/UIDropContainer';
 
 function App() {
     return (<>
@@ -12,7 +11,7 @@ function App() {
         <div className="h-screen bg-white bg-gradient-to-t from-sky-700/70 via-sky-700/50 to-sky-700/70 p-4">
             <div className="relative h-full flex flex-col">
                 {/* <Mount /> */}
-                <DropZone2 />
+                {/* <DropZone2 /> */}
                 <Section2_Main />
             </div>
         </div>
--- src/components/UI/UIDropContainer.tsx
+++ src/components/UI/UIDropContainer.tsx
@@ -1,10 +1,6 @@
-import { HTMLAttributes, useRef, useState } from "react";
-import { atom, PrimitiveAtom, useAtom, useAtomValue } from "jotai";
+import { HTMLAttributes, useRef } from "react";
+import { PrimitiveAtom, useAtom } from "jotai";
 import { classNames } from "@/utils/classnames";
-import { useUpdateAtom } from "jotai/utils";
-import { orgImgAtom } from "@/store/store";
-import { toastWarning } from "./UiToaster";
-import { IconImagePlus } from "./UIIcons";
 
 type UIDropContainerProps = {
     onDropped: (files: FileList) => void;
@@ -17,7 +13,7 @@ export function UIDropContainer({ onDropped, accept, className, children, active
     const inputRef = useRef<HTMLInputElement>(null);
     return (
         <label
-            className={classNames(`absolute inset-0`, className)}
+            className={classNames(`inline-block`, className)}
 
             onDragOver={(event) => {
                 event.preventDefault();
@@ -48,74 +44,3 @@ export function UIDropContainer({ onDropped, accept, className, children, active
         </label>
     );
 }
-
-export function DropZone2() {
-    const setOrgImg = useUpdateAtom(orgImgAtom);
-    const [activeAtom] = useState(atom(false));
-    const active = useAtomValue(activeAtom);
-
-    async function handleDrop(files: FileList) {
-        if (!files.length) { return; }
-        try {
-            console.log('files', files);
-        } catch (error) {
-            setOrgImg(null);
-            toastWarning((error as Error)?.message || 'Failed to load image');
-        }
-    }
-
-    return (
-        <div className="absolute inset-0">
-
-            <div className={`absolute left-4 top-0.5 pb-0.5 text-xs ${active ? 'text-slate-50 font-bold' : 'text-slate-100'}`}>
-                {active ? 'Drop' : 'Load image'}
-            </div>
-            
-            <UIDropContainer
-                className={`w-full h-full ${active ? 'bg-green-700' : 'bg-slate-400'} border-slate-500 border rounded cursor-pointer`}
-                onDropped={handleDrop}
-                activeAtom={activeAtom}
-            >
-                <IconImagePlus className={`${active ? 'text-green-600/30' : 'text-slate-300'}   pointer-events-none`} />
-            </UIDropContainer>
-        </div>
-    );
-}
-
-
-// export function UIDropContainer({ onDropped, accept, className, children, activeAtom, ...rest }: UIDropContainerProps & HTMLAttributes<HTMLLabelElement>) {
-//     const [dropActive, setDropActive] = useAtom(activeAtom);
-//     const inputRef = useRef<HTMLInputElement>(null);
-//     return (
-//         <label
-//             className={classNames(`inline-block`, className)}
-
-//             onDragOver={(event) => {
-//                 event.preventDefault();
-//                 event.stopPropagation();
-//                 !dropActive && setDropActive(true);
-//             }}
-
-//             onDrop={(event) => {
-//                 event.preventDefault();
-//                 event.stopPropagation();
-//                 setDropActive(false);
-//                 onDropped(event.dataTransfer.files);
-//             }}
-
-//             onDragEnter={(event) => setDropActive(true)}
-//             onDragLeave={(event) => setDropActive(false)}
-
-//             {...rest}
-//         >
-//             <input
-//                 ref={inputRef}
-//                 type="file"
-//                 className="hidden"
-//                 accept={accept}
-//                 onChange={() => inputRef.current?.files && onDropped(inputRef.current?.files)}
-//             />
-//             {children}
-//         </label>
-//     );
-// }
