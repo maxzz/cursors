2cf047db676b4f97a2477852d567a993a9468763
Max Zakharzhevskiy <maxzz@msn.com>
2022-12-08 20:40:25 -0800

    WIP. DialogIntroduction. update layout. 2

--- src/components/Section7_Dialogs/DialogIntroduction.tsx
+++ src/components/Section7_Dialogs/DialogIntroduction.tsx
@@ -7,18 +7,22 @@ import { IconGithub } from "@ui/UIIcons";
 function IntroductionBody({ editorData, setShow = (v: boolean) => { } }: { editorData: number; setShow?: (v: boolean) => void; }) {
     return (
         <div className={classNames(
-            "min-w-[400px] min-h-[480px] bg-gray-200 rounded overflow-hidden", //w-[460px] h-[640px]
-            "grid grid-rows-[min-content,1fr,min-content]")}
+            "min-w-[400px] min-h-[480px] bg-gray-200 rounded overflow-hidden",
+            "grid grid-rows-[auto,1fr,auto]")}
         >
             {/* Header */}
-            <div className="px-2 py-3 text-xl font-bold leading-tight tracking-tight border-slate-500 border-b">
+            <div className="p-4 text-xl font-bold leading-tight tracking-tight border-slate-500 border-b">
                 Instructions
             </div>
 
             {/* Body */}
-            <div className="min-w-[16rem] min-h-[6rem] p-2 text-sm">
-                <div className="w-6 text-center border-slate-400 border rounded">{editorData}</div>
-                <ul className="list-disc space-y-2">
+            <div className="min-w-[16rem] min-h-[6rem] p-4 text-sm">
+                {/* Number */}
+                <div className="w-6 text-center border-slate-400 border rounded">
+                    {editorData}
+                </div>
+
+                <ul className="mt-4 px-4 list-disc space-y-1">
                     <li className="">Upload an image to create a cursor from</li>
                     <li className="">Select the desired cursor size</li>
                     <li className="">Apply transformations</li>
