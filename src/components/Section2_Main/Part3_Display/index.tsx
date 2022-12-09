dba047bb3b8606521aa9d034a79f1c42ce213102
Max Zakharzhevskiy <maxzz@msn.com>
2022-12-09 14:33:34 -0800

    WIP. Part3_Display. update layout. 3

--- src/components/Section2_Main/Part3_Display/index.tsx
+++ src/components/Section2_Main/Part3_Display/index.tsx
@@ -12,12 +12,12 @@ function Frame3D({ children, ...rest }: HTMLAttributes<HTMLElement>) {
 
 export function Part3_Display() { //CursorTester
     return (
-        <div className="min-h-[400px] flex justify-center">
-            
+        <div className="flex justify-center space-x-4">
+
             <div className="bg-slate-300 border-b-slate-400/30 border-r-slate-400/30 border-slate-300 rounded-3xl border-[16px]">
                 <div className="p-4 bg-slate-900 border-t-slate-400/90 border-l-slate-400/90 rounded-[12px] border-8">
 
-                    <div className="flex items-center justify-center gap-x-4">
+                    <div className="min-h-[340px]">
                         <div className="w-36 h-36 bg-white border-slate-900 border"></div>
                         <div className="w-36 h-36 bg-black border-slate-500 border"></div>
                     </div>
